8dd2a0978e6242458734a71eb59bb99e49c8afff
senan25252 <senan.yusubov45@gmail.com>
2025-03-25 12:37:28 +0400

    bug fix & rearrangment & better movement
    Bug: all GameObjects uses others
    property

--- base.js
+++ base.js
@@ -1,40 +1,37 @@
-let drawObjects = []
-drawObjects.push(`<script src="base.js"></script>`);
+let drawObjects = [];
 
-class GameObject{
-    static allObjects = [];
+
+
+class GameObject {
     constructor(source, name) {
         this.sourceImage = source;
         this.name = name;
-        drawObjects.push(`<img src="${this.sourceImage}" style="position: absolute;" id="${this.name}">`);
+        this.createElement();
+        this.transform = {
+            position: { x: 10, y: 10 },
+            rotation: { z: 0 },
+            scale: { x: 100, y: 100 },
+        };
         this.drawAtTransform();
     }
 
-    transform = {
-        position: {
-            x: 10,
-            y: 10,
-        },
-        rotation: {
-            z: 0,
-        },
-        scale: {
-            x: 100,
-            y: 100,
-        },
-
-    };
-
-    drawAtTransform() {
-        document.getElementById("view").innerHTML = drawObjects.join("");
-        const obj = document.getElementById(this.name);
-        obj.style.top = `${-this.transform.position.y}px` ;
-        obj.style.left = `${this.transform.position.x}px` ;
-        obj.width = this.transform.scale.x;
-        obj.height = this.transform.scale.y;
+    createElement() {
+        const img = document.createElement("img");
+        img.src = this.sourceImage;
+        img.style.position = "absolute";
+        img.id = this.name;
+        document.getElementById("view").appendChild(img);
+        this.obj = img;
     }
 
-    isSolid = false;  
+    drawAtTransform() {
+        if (!this.obj) return;
+
+        this.obj.style.top = `${this.transform.position.y}px`;
+        this.obj.style.left = `${this.transform.position.x}px`;
+        this.obj.width = this.transform.scale.x;
+        this.obj.height = this.transform.scale.y;
+    }
 }
 
 const obj = new GameObject("square.png", "deneme");
@@ -42,24 +39,43 @@ obj.transform.position.x = 10;
 obj.transform.position.y = 0;
 obj.drawAtTransform();
 
+const obj2 = new GameObject("square.png", "deneme2");
+obj2.transform.position.x = 80;
+obj2.transform.position.y = 80;
+obj2.drawAtTransform();
 
-document.addEventListener("keydown", function(event) {
-    if(event.key == "w") {
-        obj.transform.position.y += 5;
+let movingRight = false;
+let movingLeft = false;
+let movingUp = false;
+let movingDown = false;
+
+move();
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "d") movingRight = true;
+    if (event.key === "a") movingLeft = true;
+    if (event.key === "w") movingUp = true;
+    if (event.key === "s") movingDown = true;
+});
+
+document.addEventListener("keyup", function (event) {
+    if (event.key === "d") movingRight = false;
+    if (event.key === "a") movingLeft = false;
+    if (event.key === "w") movingUp = false;
+    if (event.key === "s") movingDown = false;
+});
+
+function move() {
+    // 60 FPS güncelleme (16ms aralıklarla çalışır)
+    setInterval(() => {
+        if (movingRight) obj.transform.position.x += 5; // Her 16ms'de bir 5px hareket
+        if (movingLeft) obj.transform.position.x -= 5;
+        if (movingUp) obj.transform.position.y -= 5;
+        if (movingDown) obj.transform.position.y += 5;
+    
         obj.drawAtTransform();
-    }
-    if(event.key == "s") {
-        obj.transform.position.y -= 5;
-        obj.drawAtTransform();
-    }
-    if(event.key == "d") {
-        obj.transform.position.x += 5;
-        obj.drawAtTransform();
-    }
-    if(event.key == "a") {
-        obj.transform.position.x -= 5;
-        obj.drawAtTransform();
-    }
-    console.log(`${obj.transform.position.y} ${obj.transform.position.x}`);
-})
+    }, 16); // Yaklaşık 60 FPS
+        
+}
+
 
